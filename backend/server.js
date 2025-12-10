b3ef620e91baac81e83e20b23e1c996b78bb5619
siddhant agrawal <siddhantagrawal001@gmail.com>
2025-12-10 20:19:46 +0530

    add the code for spinning the server

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import documentRoutes from './routes/document.routes.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL ,
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/documents', documentRoutes);
+
+
+
+app.listen(PORT, () => {
+    console.log(` Server is running on http://localhost:${PORT}`);
+});
